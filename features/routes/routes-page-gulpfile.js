68c1aba381f6e151dcc9ed64571dc0d5926bf513
Xavier Boubert <xavier@boubert.fr>
2016-09-07 13:01:11 +0200

    fix(outes page gulpfile): use new $gulp.dist() feature

--- features/routes/routes-page-gulpfile.js
+++ features/routes/routes-page-gulpfile.js
@@ -9,14 +9,14 @@ module.exports = function($gulp) {
   $gulp.task('page', function(done) {
     $gulp
       .src('node_modules/page/page.js')
-      .pipe($gulp.dest('./public/vendor'))
+      .pipe($gulp.dist('vendor'))
       .pipe(sourcemaps.init())
       .pipe(uglify())
       .pipe(rename({
         extname: '.min.js'
       }))
       .pipe(sourcemaps.write('./'))
-      .pipe($gulp.dest('./public/vendor'))
+      .pipe($gulp.dist('vendor'))
       .on('end', done);
   });
 
